cc843f5183948c39a1a2133641d10f75c144967a
jieny <576420147@qq.com>
2023-05-11 12:25:11 +0800

    20230511122511

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,6 @@
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
